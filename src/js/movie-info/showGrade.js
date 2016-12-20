295c23be5b24d2781bf556d81646d4df28506024
동아 <dongakim90@gmail.com>
2016-12-20 16:03:05 +0900

    completed 'Show Grade'

--- src/js/movie-info/showGrade.js
+++ src/js/movie-info/showGrade.js
@@ -4,17 +4,26 @@
 
     var movie_info_comment_rating_group = document.querySelector('.movie-comment-rating-group');
     var movie_info_labels = movie_info_comment_rating_group.querySelectorAll('label');
+    var movie_info_output = movie_info_comment_rating_group.querySelector('output');
+
+    // console.log(movie_info_comment_rating_group);
+    // console.log(movie_info_labels);
+    // console.log(movie_info_output);
 
     var showStarRatingGrade = function(e) {
       setTimeout(function(){
         var movie_info_rated_star = $('input:radio[name=rating]:checked').val();
-        console.log(movie_info_rated_star);
+
+        movie_info_output.innerHTML = movie_info_rated_star;
+
+        // console.log(movie_info_rated_star);
+        // console.log(movie_info_output);
+
       },50);
     };
 
 
-    console.log(movie_info_comment_rating_group);
-    console.log(movie_info_labels);
+
 
 
     for ( var i = 0, l = movie_info_labels.length; i < l; i++ ) {
